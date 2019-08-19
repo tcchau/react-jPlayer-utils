b733f3522ab1fd4ee3c1fcb058590289bd72f252
Eugene Sysmanov <appigram@gmail.com>
2019-08-19 18:29:58 +0300

    Fix for convertTime

--- src/convertTime.js
+++ src/convertTime.js
@@ -1,20 +1,20 @@
 export default (seconds, timeFormats) => {
   if (!Number.isInteger(seconds)) {
-    return ''
+    return '';
   }
-  const myTime = new Date(seconds * 1000)
+  const myTime = new Date(seconds * 1000);
 
-  const hour = myTime.getUTCHours()
-  const min = timeFormats.showHour ? myTime.getUTCMinutes() : myTime.getUTCMinutes() + (hour * 60)
-  const sec = timeFormats.showMin ? myTime.getUTCSeconds() : myTime.getUTCSeconds() + (min * 60)
-  const strHour = (timeFormats.padHour && hour < 10) ? `0${hour}` : hour
-  const strMin = (timeFormats.padMin && min < 10) ? `0${min}` : min
-  const strSec = (timeFormats.padSec && sec < 10) ? `0${sec}` : sec
+  const hour = myTime.getUTCHours();
+  const min = timeFormats.showHour ? myTime.getUTCMinutes() : myTime.getUTCMinutes() + (hour * 60);
+  const sec = timeFormats.showMin ? myTime.getUTCSeconds() : myTime.getUTCSeconds() + (min * 60);
+  const strHour = (timeFormats.padHour && hour < 10) ? `0${hour}` : hour;
+  const strMin = (timeFormats.padMin && min < 10) ? `0${min}` : min;
+  const strSec = (timeFormats.padSec && sec < 10) ? `0${sec}` : sec;
 
-  let strTime = ''
-  strTime += timeFormats.showHour ? strHour + timeFormats.sepHour : ''
-  strTime += timeFormats.showMin ? strMin + timeFormats.sepMin : ''
-  strTime += timeFormats.showSec ? strSec + timeFormats.sepSec : ''
+  let strTime = '';
+  strTime += timeFormats.showHour ? strHour + timeFormats.sepHour : '';
+  strTime += timeFormats.showMin ? strMin + timeFormats.sepMin : '';
+  strTime += timeFormats.showSec ? strSec + timeFormats.sepSec : '';
 
-  return strTime
-}
+  return strTime;
+};
